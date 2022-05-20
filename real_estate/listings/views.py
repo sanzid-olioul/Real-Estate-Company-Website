a987ddc9de31efe718238f69c9d8bd27abcfc322
Sanzid Olioul <sanzid94@hotmail.com>
2022-05-21 00:33:04 +0600

    Templates added

--- real_estate/listings/views.py
+++ real_estate/listings/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    print('hii there')
+    return render(request,'home.html') 
+    # return HttpResponse("hellow there")
